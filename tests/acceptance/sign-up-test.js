f24bde8c37bde0bffa7accf1f785d8d74211b5a4
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-02-09 10:02:44 -0200

    WIP stubbing fb request through Pretender

--- tests/acceptance/sign-up-test.js
+++ tests/acceptance/sign-up-test.js
@@ -1,25 +1,32 @@
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 
-var App;
+var App, server;
 
 describe('Sign up', function() {
   beforeEach(function(){
     App = startApp();
+    server = new Pretender(function() {
+      this.get("https://www.facebook.com/dialog/oauth", function(request) {
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({"authorizationCode":"somecode"})];
+      });
+    });
   });
   
   afterEach(function() {
+    server.shutdown();
     Ember.run(App, 'destroy');
   });
 
   describe('with Facebook', function(){
     beforeEach(function(){
-      visit('/').then(function() {
+      visit('/login').then(function() {
         click( $("button:contains('Login or Sign-up with Facebook')") );             
       });
     });
 
     it('shows success message', function() {
+
       expect(find('.notice').text()).to.equal('Logged in to Facebook successfully');
     });
   });
